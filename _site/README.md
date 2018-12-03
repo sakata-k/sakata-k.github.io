d1199c58c7f9f8da575cc887c06a5e3fc1c158b8
tomato03 <k.sakata@amd.tokyo>
2018-12-03 12:08:14 +0900

    tuika

---
+++ _site/README.md
@@ -0,0 +1,5 @@
+酒田和宏のポートフォリオサイトです。
+よかったら見ていってください！
+
+Welcome to Kazuhiro Sakata page!
+Please look at my Portfolio page.
---
+++ _site/assets/js/script.js
@@ -0,0 +1,51 @@
+//アンカーリンク スムーズに
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+
+$(document).ready( function(){
+  $('.clip_left').addClass("on");
+});
+
+$(function(){
+  $('.wrapper #top').on('inview', function() {
+    $('.wrapper #top').addClass("active");
+  });
+  $('.wrapper #art').on('inview', function() {
+    $('.wrapper #art').addClass("active");
+    $('#art li').each(function(i) {
+      $(this).delay(500 * i).animate({opacity:1,top:0});
+    });
+  });
+  $('.wrapper #about').on('inview', function() {
+    $('.wrapper #about').addClass("active");
+    $('#about').delay(4000).queue(function(){
+      $('#about').addClass("slide");
+    });
+    $('#about .skill').delay(6000).queue(function(){
+      $('#about .skill').addClass("on");
+    });
+  });
+  $('.wrapper #contact').on('inview', function() {
+    $('.wrapper #contact').addClass("active");
+  });
+});
+
+//ヘッダー コールバック ハンバーガー制御
+$(window).on("load", function() {
+  $(".header_inner").load("/header.html #header",   function (){
+    $('#header .hum').click(function(){
+      $("#header").toggleClass("open");
+    });
+    $('.header_list a').click(function(){
+      $("#header").removeClass();
+    });
+  });
+});
